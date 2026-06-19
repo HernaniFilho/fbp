aef2004f97f806b8e67a6dc754caa8061aed497d
HernaniFilho <hernanipcf@gamil.com>
2026-06-19 20:12:32 -0300

    Staff tab started

--- frontend/src/routes/_app.staff.tsx
+++ frontend/src/routes/_app.staff.tsx
@@ -1,9 +1,37 @@
+import { Tabs, TabsList, TabsTrigger } from '#/components/ui/tabs'
+import { useIsMobile } from '#/hooks/use-mobile'
 import { createFileRoute } from '@tanstack/react-router'
+import { Activity, useState } from 'react'
 
 export const Route = createFileRoute('/_app/staff')({
   component: Staff,
 })
 
 function Staff() {
-  return <div>WIP see staff here</div>
+  const [currentTab, setCurrentTab] = useState('all')
+  const isMobile = useIsMobile()
+
+  return (
+    <div className="mx-6 mt-6 flex flex-col md:flex-row gap-6">
+      <Tabs
+        value={currentTab}
+        onValueChange={setCurrentTab}
+        orientation={isMobile ? 'horizontal' : 'vertical'}
+      >
+        <TabsList variant="line">
+          <TabsTrigger value="all">All</TabsTrigger>
+          <TabsTrigger value="register">Register</TabsTrigger>
+        </TabsList>
+      </Tabs>
+
+      <div className="flex-1">
+        <Activity mode={currentTab === 'all' ? 'visible' : 'hidden'}>
+          <h1>You are viewing all staff</h1>
+        </Activity>
+        <Activity mode={currentTab === 'register' ? 'visible' : 'hidden'}>
+          <h1>You are registering a new staff member</h1>
+        </Activity>
+      </div>
+    </div>
+  )
 }
